52bfc5e9bf0f561c9ec2d3b02818d40911ee945b
Robin Hack <hack.robin@gmail.com>
2017-01-06 15:07:21 +0100

    c: End script when no passwd given

--- c.sh
+++ c.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 CONTENT=$(gpg2 --output - --decrypt servers.txt.gpg 2> /dev/null)
+RET=$?
+if [[ $RET -ne 0 ]]; then
+    exit 1
+fi
 LIST=$(cut -d':' -f1 <(printf "$CONTENT"))
 SERVER=$(zenity --height=400 --list --column Server $LIST)
 if [[ $SERVER == '' ]]; then
