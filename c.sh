c6f17b99986eff22a801009d38a89e34be250ee9
Robin Hack <hack.robin@gmail.com>
2017-01-07 22:29:47 +0100

    Added single sign-on support.
    Still not secure but it's better little bit.

--- c.sh
+++ c.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-CONTENT=$(gpg2 --output - --decrypt servers.txt.gpg 2> /dev/null)
+if [[ $MPASS == "" ]]; then
+    stty_orig=$(stty -g)
+    echo -n "password: "
+    stty -echo
+    read MPASS
+    stty $stty_orig
+    MPASS="$MPASS" exec $0
+fi
+
+CONTENT=$(gpg2 --batch --passphrase $MPASS --output - --decrypt servers.txt.gpg 2> /dev/null)
 RET=$?
 if [[ $RET -ne 0 ]]; then
     exit 1
@@ -13,4 +22,4 @@ fi
 
 PASS=$(grep $SERVER  <(printf "$CONTENT") | cut -d':' -f2)
 sshpass -p$PASS ssh -o StrictHostKeyChecking=no root@$SERVER
-exec $0
+MPASS="$MPASS" exec $0
